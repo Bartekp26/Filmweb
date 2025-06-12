e27a5e1b5813658b5c3790e1d1f20b03dc21ff0c
BADusty <bartek.drosik@gmail.com>
2025-06-12 15:54:47 +0200

    ProfileView + TopBar Update

--- Filmweb/ViewModel/MainVM.cs
+++ Filmweb/ViewModel/MainVM.cs
@@ -33,28 +33,36 @@ namespace Filmweb.ViewModel
         public ICommand SwitchToLoginCommand { get; }
         public ICommand LogInCommand { get; }
         public ICommand LogOutCommand { get; }
+        public ICommand NavigateToHomeCommand { get; }
+        public ICommand NavigateToProfileCommand { get; }
 
         public bool IsLoginOrRegisterView => CurrentView is LoginView || CurrentView is RegisterView;
 
         private readonly LoginView _loginView;
         private readonly RegisterView _registerView;
         private readonly HomeView _homeView;
+        private readonly ProfileView _profileView;
 
         public MainVM()
         {
             var loginVM = new LoginVM(this);
             var registerVM = new RegisterVM(this);
             var homeVM = new HomeVM(this);
+            var profileVM = new ProfileVM(this);
 
             _loginView = new LoginView { DataContext = loginVM };
             _registerView = new RegisterView { DataContext = registerVM };
             _homeView = new HomeView { DataContext = homeVM };
+            _profileView = new ProfileView { DataContext = profileVM };
 
             SwitchToRegisterCommand = new RelayCommand(_ => CurrentView = _registerView, p => true);
             SwitchToLoginCommand = new RelayCommand(_ => CurrentView = _loginView, p => true);
 
             LogInCommand = new RelayCommand(_ => ExecuteLogin(), p => true);
             LogOutCommand = new RelayCommand(_ => ExecuteLogout(), p => true);
+
+            NavigateToHomeCommand = new RelayCommand(_ => CurrentView = _homeView, p => true);
+            NavigateToProfileCommand = new RelayCommand(_ => CurrentView = _profileView, p => true);
 
             CurrentView = _loginView;
         }
