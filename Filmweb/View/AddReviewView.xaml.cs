2d8c41541bce760493e85a7d907a82d1e87d7b34
BADusty <bartek.drosik@gmail.com>
2025-06-21 22:45:23 +0200

    UI Fixes + Extended.Wpf.Toolkit

--- Filmweb/View/AddReviewView.xaml.cs
+++ Filmweb/View/AddReviewView.xaml.cs
@@ -23,5 +23,39 @@ namespace Filmweb.View
         {
             InitializeComponent();
         }
+
+        private void IntegerUpDown_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out _);
+        }
+
+        private void IntegerUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (sender is Xceed.Wpf.Toolkit.IntegerUpDown intUpDown && intUpDown.Value.HasValue)
+            {
+                int value = intUpDown.Value.Value;
+
+                if (value < intUpDown.Minimum)
+                    intUpDown.Value = (int)intUpDown.Minimum;
+                else if (value > intUpDown.Maximum)
+                    intUpDown.Value = (int)intUpDown.Maximum;
+            }
+        }
+
+        private void IntegerUpDown_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string text = (string)e.DataObject.GetData(typeof(string));
+                if (!int.TryParse(text, out _))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
     }
 }
--- Filmweb/ViewModel/AddReviewVM.cs
+++ Filmweb/ViewModel/AddReviewVM.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Filmweb.ViewModel
 {
-    public class AddReviewVM
+    public class AddReviewVM : INotifyPropertyChanged
     {
         private readonly MainVM _mainVM;
+
+        private double _rating;
+        public double Rating
+        {
+            get => _rating;
+            set
+            {
+                if (_rating != value)
+                {
+                    _rating = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public AddReviewVM(MainVM mainVM) 
         {
             _mainVM = mainVM;
-        }   
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
